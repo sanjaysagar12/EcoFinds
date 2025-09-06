564cabf46bf0e81696d5b82b0c449b5ce2d05666
sanjaysagar12 <me@sanjaysagar.com>
2025-09-06 11:49:18 +0530

    feat(product): add Product module, controller, and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule, ProductModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
